0b378556aa758c027e8d495649f7d408c19dda92
orosz.eszter <orosz.eszter@28-DIAK7>
2024-11-11 10:34:35 +0100

    Add project files.

---
+++ ragadozo_novenyevohalak/Fish.cs
@@ -0,0 +1,71 @@
+﻿namespace ragadozo_novenyevohalak
+{
+    internal class Fish
+    {
+        private float weight;
+        private bool weightIsSet = false;
+        private bool predator;
+        private bool predatorIsSet = false;
+        private int top;
+        private int depth;
+        private string species;
+
+        public float Weight
+        {
+            get => weight;
+            set
+            {
+                if (value < .5f || value > 40f)
+                    throw new Exception("a hal súlya nem megfelelő");
+
+                if (weightIsSet && ( value > weight * 1.1 || value < weight * 0.9))
+                    throw new Exception("a hal súlya ennyivel nem változhat");
+                weight = value;
+                weightIsSet = true;
+            }
+        }
+        public bool Predator { get; private set; }
+
+        public int Top 
+        { 
+            get => top;
+            set
+            {
+                if (value < 0 || value > 400)
+                    throw new Exception("invalid érték");
+                top = value;
+            }
+        }
+        public int Depth
+        {
+            get => depth;
+            set
+            {
+                if (value < 10 || value > 400)
+                    throw new Exception("invalid érték");
+                depth = value;
+            }
+        }
+        public string Species
+        {
+            get => species;
+            set
+            {
+                if (value is null)
+                    throw new Exception("nem lehet null");
+                if (value.Length < 3 || value.Length > 30)
+                    throw new Exception("Nem megfelelő hossz.");
+                species = value;
+            }
+        }
+
+        public Fish(float weight, bool predator, int top, int depth, string species)
+        {
+            Weight = weight;
+            Predator = predator;
+            Top = top;
+            Depth = depth;
+            Species = species;
+        }
+    }
+}
---
+++ ragadozo_novenyevohalak/Program.cs
@@ -0,0 +1,53 @@
+﻿using ragadozo_novenyevohalak;
+
+string[]  fajtak = ["harcsa", "keszeg", "ponty", "Compó", "Kárász", "Sügér", "Csuka", "Amur", "Csüllő", "Csík", "Szürkeharcsa"];
+List<Fish> fishLake = new List<Fish>();
+Fish fish = new Fish
+(
+    species: fajtak[Random.Shared.Next(fajtak.Length)],
+    predator: Random.Shared.Next(100) < 10,
+    top: Random.Shared.Next(0,400),
+    depth: Random.Shared.Next(10, 400),
+    weight: Random.Shared.Next(1, 80) / 2F
+);
+ 
+fish.Weight += 1;
+
+Console.WriteLine($"Fish is {fish.Weight::0.00} kg");
+Console.WriteLine($"{(fish.Predator ? "predator" : "vegie")}");
+
+for (int i = 0; i < 100; i++)
+{
+    fishLake.Add(new(
+        species: fajtak[Random.Shared.Next(fajtak.Length)],
+        predator: Random.Shared.Next(100) < 10,
+        top: Random.Shared.Next(0, 400),
+        depth: Random.Shared.Next(10, 400),
+        weight: Random.Shared.Next(1, 80) / 2F));
+    
+}
+for (int i = 0;i < 100; i++)
+{
+    Fish x = fishLake[Random.Shared.Next(fishLake.Count)];
+    Fish y = fishLake[Random.Shared.Next(fishLake.Count)];
+
+    if (x.Top > y.Top) (x, y) = (y, x);
+    Console.WriteLine("selected fish:");
+    Console.WriteLine();
+
+
+    if (x.Predator != y.Predator)
+    {
+        if (x.Top + x.Depth >= y.Top)
+        {
+            fishLake.Remove(y);
+            if (x.Weight < 40) x.Weight *= 1.05F;
+
+        }
+        else
+        {
+            fishLake.Remove(x);
+            if (y.Weight < 40) x.Weight *= 1.05F;
+        }
+    }
+}
